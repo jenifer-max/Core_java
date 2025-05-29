a791997ab9a40a473c50352e29700192545cd057
Jenifer <jeniferdon27@gmail.com>
2025-05-29 13:01:22 +0530

    Pattern_4

---
+++ pattern_4.java
@@ -0,0 +1,15 @@
+class pattern_4{
+  
+   public static void main(String[] args)  
+   {  
+   int i,j, n=6;  
+   for(i=1;i<=n;i++)  
+   {  
+   for(j=1;j<=i;j++)  
+   {  
+       System.out.print(" *");  
+   }  
+       System.out.println("");  
+   }  
+   } 
+ } 
