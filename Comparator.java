682830ad80e98d180b61fcf1a057065ec199cc05
Jenifer <jeniferdon27@gmail.com>
2025-06-17 15:15:00 +0530

    Comparator Methods

---
+++ Comparator.java
@@ -0,0 +1,11 @@
+class Comparator{
+public static void main(String arg[]){
+System.out.println("A".compareTo("M"));
+System.out.println("M".compareTo("A"));
+System.out.println("A".compareTo("A"));
+}
+
+
+
+
+}
